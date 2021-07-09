1d5a1a36d1b99b1dc85bb4cccd31c39eadfd0cce
Nathan Watts <nathan@naykel.com.au>
2021-07-10 03:47:11 +1000

    fix view

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,10 +2,14 @@
 
 @section('content')
 
-    <h1>{{ $title }}</h1>
+<div class="container py-3">
+    <div class="col-md-50 max">
+        <h1>{{ $title }}</h1>
 
-    <x-contactit-contact>
-        {{-- remove top margin if adding $slot content --}}
-    </x-contactit-contact>
+        <x-contactit-contact>
+            {{-- remove top margin if adding $slot content --}}
+        </x-contactit-contact>
+    </div>
+</div>
 
 @endsection
