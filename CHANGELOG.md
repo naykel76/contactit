e86e10d40feb207dc2aa3715475f3caee79fdf7a
naykel76 <32632005+naykel76@users.noreply.github.com>
2021-12-01 10:21:38 +1000

    v0.1.1 updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,17 @@
 # Changelog
 
+## 0.1.1
+
+- update layouts
+
 ## 0.0.4
 
--update formit components
+- update formit components
 
 ## 0.0.3
 
 - fix layout
-  
+
 ## 0.0.2
 
 - fix layout
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,15 +1,13 @@
-@extends('gotime::layouts.' . config('naykel.template'))
+<x-gotime-app-layout layout="{{ config('naykel.template') }}">
 
-@section('content')
+    <div class="container py-3">
+        <div class="col-md-50 max">
+            <h1>{{ $title }}</h1>
 
-<div class="container py-3">
-    <div class="col-md-50 max">
-        <h1>{{ $title }}</h1>
-
-        <x-contactit-contact>
-            {{-- remove top margin if adding $slot content --}}
-        </x-contactit-contact>
+            <x-contactit-contact>
+                {{-- remove top margin if adding $slot content --}}
+            </x-contactit-contact>
+        </div>
     </div>
-</div>
 
-@endsection
+</x-gotime-app-layout>
